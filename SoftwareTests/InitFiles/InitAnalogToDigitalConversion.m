adc0aceaa974a2421a6d24bcc0701f277c58b2f9
Valentin Boutrouche <vboutrou@mathworks.com>
2024-09-10 10:19:14 -0400

    Modify CI

--- SoftwareTests/InitFiles/InitAnalogToDigitalConversion.m
+++ SoftwareTests/InitFiles/InitAnalogToDigitalConversion.m
@@ -3,6 +3,9 @@
 KnownIssuesID = "MATLAB:minrhs";
 % ---- Pre-run commands -----
 audioread = @(x) NewAudioRead();
+audioplayer = @(x) disp("Create audio player");
+play = @(x) disp("Playing audio");
+stop = @(x) disp("Stop audio player");
 function varargout=NewAudioRead(varargin)
 load(fullfile(currentProject().RootFolder,"SoftwareTests\InitFiles\InitAnalogToDigitalConversion.mat"));
 varargout={JazzSignal,JazzSamplingFreq};
--- SoftwareTests/InitFiles/InitFilteringIntro.m
+++ SoftwareTests/InitFiles/InitFilteringIntro.m
@@ -2,7 +2,9 @@
 % ---- Known Issues     -----
 KnownIssuesID = "MATLAB:minrhs";
 % ---- Pre-run commands -----
+audioplayer = @(x) disp("Create audio player");
 play = @(x) disp("Playing audio");
+stop = @(x) disp("Stop audio player");
 audioread = @(x) NewAudioRead();
 function varargout=NewAudioRead(varargin)
 load(fullfile(currentProject().RootFolder,"SoftwareTests\InitFiles\InitFilteringIntro.mat"));
