02660b415f67790fed1cfc685bd483463b61959c
Valentin Boutrouche <vboutrou@mathworks.com>
2024-09-27 16:11:06 -0400

    Modify CI

--- SoftwareTests/PreFiles/PreFilterDesign.m
+++ SoftwareTests/PreFiles/PreFilterDesign.m
@@ -3,4 +3,9 @@ OnSolution = true;
 % ---- Known Issues     -----
 KnownIssuesID = "MATLAB:minrhs";
 % ---- Pre-run commands -----
- play = @(x) disp("Playing audio");
+play = @(x) disp("Playing audio");
+audioread = @(x) NewAudioRead(x);
+function varargout=NewAudioRead(varargin)
+load(fullfile(currentProject().RootFolder,"SoftwareTests","PreFiles","PreFilterDesign.mat"));
+varargout={Signal,SamplingFreq};
+end
